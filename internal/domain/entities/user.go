816be1b91aab991c05be7bac44243c24f6134bec
DanielSinnc <daniel@sinnc.com.br>
2025-08-27 20:04:02 -0300

    Revert "first commit"
    This reverts commit a5362508769436a0a553c555ded07b7107a06993.

--- internal/domain/entities/user.go
+++
@@ -1,6 +0,0 @@
-package entities
-
-type User struct {
-	Username string `db:"username"`
-	Password string `db:"password"`
-}
--- internal/domain/models/login_result.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type LoginResult struct {
-	Ok      bool   `json:"ok"`
-	Message string `json:"message"`
-}
